b23a0145d1b2a61f777c1648cfa71dbbf13ff0ab
khalosamilo <mohasamilol@gmail.com>
2023-02-09 13:53:02 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
+import "./App.css";
+
+function App() {
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [placeholder, setPlaceholder] = useState(
+    "Search Bears with Paint Brushes the Starry Night, painted by Vincent Van Gogh.."
+  );
+  const configuration = new Configuration({
+    apiKey: import.meta.env.VITE_Open_AI_Key,
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  const generateImage = async () => {
+    setPlaceholder(`Search ${prompt}..`);
+    setLoading(true);
+    const res = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "512x512",
+    });
+    setLoading(false);
+    setResult(res.data.data[0].url);
+  };
+  return (
+
+    <div className="app-main">
+  {loading ? (
+    <>
+      <h2>Generating..Please Wait..</h2>
+      <div class="lds-ripple">
+        <div></div>
+        <div></div>
+      </div>
+    </>
+  ) : (
+    <>
+      <h2>Mahmood pics</h2>
+      <div className="textarea-container">
+        <textarea
+          className="app-input"
+          placeholder={placeholder}
+          onChange={(e) => setPrompt(e.target.value)}
+        />
+      </div>
+      <div className="button-container">
+        <button className="solver-btn">
+          <a href="https://mahmood-solver.netlify.app" target="_blank">⫷solver</a>
+        </button>
+        <button className="generate-btn" onClick={generateImage}>Generate</button>
+      </div>
+      {result.length > 0 ? (
+        <img className="result-image" src={result} alt="result" />
+      ) : (
+        <></>
+      )}
+    </>
+  )}
+</div>
+  )
+}
+
+export default App;
